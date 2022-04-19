1d9d176697dfde95667c064960694d121b53148f
Ronibw <bierwirth.veronica@gmail.com>
2022-04-19 17:51:01 -0400

    added spice to the menu with forward and backward select audio queues all buttons on the menu working as intended EXCEPT PLAY

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,6 +8,9 @@ public class MenuController : MonoBehaviour
 {
     public GameObject quitPanel;
     public GameObject creditsPanel;
+    public AudioClip menuForwardSelect;
+    public AudioClip menuBackSelect;
+    public AudioSource menuAudioSource;
 
 
     // Start is called before the first frame update
@@ -26,28 +29,39 @@ public class MenuController : MonoBehaviour
     public void PlayGame()
     {
         creditsPanel.SetActive(true);
+        menuAudioSource.PlayOneShot(menuForwardSelect);
     }
 
+
+    public void OpenCreditsPanel()
+    {
+        creditsPanel.SetActive(true);
+        menuAudioSource.PlayOneShot(menuForwardSelect);
+    }
     public void CloseCreditsScreen()
     {
         creditsPanel.SetActive(false);
+        menuAudioSource.PlayOneShot(menuBackSelect);
     }
 
     public void QuitConfirmationPanel() 
     {
         quitPanel.SetActive(true);
         Time.timeScale = 0;
+        menuAudioSource.PlayOneShot(menuForwardSelect);
     }
 
     public void QuitCancel()
     {
         quitPanel.SetActive(false);
         Time.timeScale = 1;
+        menuAudioSource.PlayOneShot(menuBackSelect);
     }
 
     public void QuitApplication()
     {
         Application.Quit();
         print("Quit Game Successful");
+        menuAudioSource.PlayOneShot(menuForwardSelect);
     }
 }
