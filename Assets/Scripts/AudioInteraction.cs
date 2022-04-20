1a23132d92aa2a31edaa238b558f8839e4a0c372
Daniel W Wallace <71410943+JayPaladin@users.noreply.github.com>
2022-04-20 12:15:28 -0400

    BOOM i made my own workspace

--- Assets/Scripts/AudioInteraction.cs
+++ Assets/Scripts/AudioInteraction.cs
@@ -15,7 +15,8 @@ public class AudioInteraction : MonoBehaviour, IInteractable
     public void interact()
     {
         audioSource.PlayOneShot(audioClip);
-        CircleTrigger.enabled = true;      
+        CircleTrigger.enabled = true;
+        Distraction_System.instance.ActivateDistraction(this.transform);
     }
 
     // Start is called before the first frame update
@@ -28,24 +29,7 @@ public class AudioInteraction : MonoBehaviour, IInteractable
         if (!TryGetComponent<AudioSource>(out audioSource))
         {
             audioSource = this.gameObject.AddComponent<AudioSource>();
-        }
-        if (!TryGetComponent<CircleCollider2D>(out CircleTrigger))
-        {
-            CircleTrigger = this.gameObject.AddComponent<CircleCollider2D>();
-        }
-        CircleTrigger.isTrigger = true;
-        CircleTrigger.enabled = false;
-        CircleTrigger.radius = radius;
-    }
-    private void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        if(timeLeft<0)
-        {
-            CircleTrigger.enabled = false;
-            timeLeft = audioClip.length;
-        }
-         
+        }      
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
