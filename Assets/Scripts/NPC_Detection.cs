ac119928cd2a0d6db66dd9997c14f25110efca91
Daniel W Wallace <71410943+JayPaladin@users.noreply.github.com>
2022-04-21 15:51:14 -0400

    boom npc detection fixed

--- Assets/Scripts/NPC_Detection.cs
+++ Assets/Scripts/NPC_Detection.cs
@@ -7,10 +7,12 @@ public class NPC_Detection : MonoBehaviour
     public bool isBeingDetected = false;
     public NPC_Behavior npcBehavior;
     public Collider2D Cone;
+    public LayerMask canDetect;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Player" && isPlayerFirst(collision.ClosestPoint(this.transform.position)))
+        if (collision.gameObject.CompareTag("Player") /*&& isPlayerFirst(collision.ClosestPoint(this.transform.position))*/)
         {
+            //Debug.Break();
             if (!PlayerHidingSystem.instance.hidden)
             {
                 isBeingDetected = true;
@@ -27,18 +29,19 @@ public class NPC_Detection : MonoBehaviour
         if (collision.tag == "Player")
         {
             isBeingDetected = false;
-
         }
     }
 
     private bool isPlayerFirst(Vector2 otherPoint)
     {
         Vector2 thisV2Pos = new Vector2(this.transform.position.x, this.transform.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, (otherPoint - thisV2Pos).normalized, 7);
-        Debug.DrawRay(this.transform.position, (otherPoint - thisV2Pos).normalized * 7, Color.red, 0.5f);
-        Debug.Log($"did i hit the player... {hit.transform.CompareTag("Player")}");
-        return (hit.transform.CompareTag("Player"));
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, (thisV2Pos - otherPoint).normalized, 7, canDetect);
+        Debug.DrawRay(this.transform.position, (otherPoint - thisV2Pos).normalized * 7, Color.red, 0.05f);
+        if(hit.collider!=null)
+        {
+            Debug.Log($"did i hit the player... {hit.transform.name},{hit.transform.tag}");
+            return (hit.transform.CompareTag("Player"));
+        }
+        return false;
     }
-
-
 }
