6e45f98c3bfab21159e577b0e9dfc2968dc81b9f
MR-888 <mohsenrahimi115@gmail.com>
2022-04-22 00:21:20 -0400

    final - final fixes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -18,7 +18,7 @@ public class Timer : MonoBehaviour
 
     private void Start()
     {
-        oneMinuteLeft = timeValue - 60;
+        oneMinuteLeft = timeValue - 10;
         oneMinuteLeftSound.PlayDelayed(oneMinuteLeft);
     }
 
